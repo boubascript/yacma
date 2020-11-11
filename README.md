472a8883f44860bbf6d534c1ef2baa0c85b8beed
Boubacar <boubacar.diallo1@macaulay.cuny.edu>
2020-11-11 16:03:41 -0500

    syllabus week (basic app running)

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # yacma
 Yet Another (Distributed) Classroom Management App 
+
+## Requirements
+* Node >=10
+
+## API aka server aka backend
+`cd api`
+`npm run dev`
+The client site  will be running at [localhost:3000](localhost:3000).
+
+## client aka frontend
+`cd client`
+`npm start`
+The server api will be running at [localhost:8080](localhost:8080).
+
+Both frontend and backend will automatically reload whenever the relevant files for each are saved.
+
+## Deploying
+
+`npm run deploy` in the folder of the service you want to deploy. (client is the default service)
---
+++ api/app.ts
@@ -0,0 +1,49 @@
+import express from "express";
+import bodyparser from "body-parser";
+import logger from "morgan";
+
+(async () => {
+  try {
+    // Initialize express
+    const app = express();
+    const port = Number(process.env.PORT) || 8080;
+    app.set("port", port);
+
+    // Middleware
+    app.use(bodyparser.urlencoded({ extended: false }));
+    app.use(bodyparser.json());
+
+    if (process.env.NODE_ENV !== "production") {
+      app.use(logger("dev"));
+      app.use(function (
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+      ) {
+        res.header("Access-Control-Allow-Origin", `http://localhost:3000`);
+        res.header(
+          "Access-Control-Allow-Headers",
+          "Origin, X-Requested-With, Content-Type, Accept"
+        );
+        next();
+      });
+    }
+
+
+    // Routes
+    app.get("/", (req: express.Request, res: express.Response) => {
+      return res.send(`Hello YACMA!`);
+    });
+
+    app.get("/api", (req: express.Request, res: express.Response) => {
+      return res.json(`YACMA. It's on the syllabus.`);
+    });
+
+    // Launch Server
+    app.listen(port, () => {
+      console.log(`📡 Server up! 📡 Listening on  http://localhost:${port}`);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+})();
