b3b80f1cf55adc2ead4b831c5bc1bcfe7197a0ff
Eileen <ehuang053@gmail.com>
2020-11-15 17:19:50 -0500

    added post and newpost component

---
+++ client/src/pages/Post/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Container from '@material-ui/core/Container';
+
+const Post: React.FunctionComponent = props => {
+  return(
+    <React.Fragment>
+      <Container >
+        <h2>
+          {props.title}
+        </h2>
+        <p>
+          {props.desc}
+        </p>
+        <div>
+          {props.files}
+        </div>
+      </Container>
+    </React.Fragment>
+  );
+}
+
+export default Post;
