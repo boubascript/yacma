1acb695883e24316fc7b1450cc65ae2f72747794
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-11-30 13:41:25 -0500

    added imports, post object, updated form

--- client/src/pages/NewPost/index.tsx
+++ client/src/pages/NewPost/index.tsx
@@ -1,59 +1,65 @@
-import React from "react";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
+import React, { useState, useEffect, useContext } from "react";
+import { Grid, TextField, Button } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import { UserContext } from "utils/auth";
+
+interface PostData {
+  title: string;
+  author: string;
+  description: string;
+  links: string;
+}
+const DEFAULT_POST_DATA: PostData = {
+  title: "",
+  author: "",
+  description: "",
+  links: "",
+};
 
 const NewPost: React.FunctionComponent = () => {
-  const [title, setTitle] = React.useState("");
-  const [desc, setDesc] = React.useState("");
+  const [postData, setPostData] = useState<PostData>(DEFAULT_POST_DATA);
 
-  const handleTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTitle(event.target.value);
-  };
-
-  const handleDesc = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setDesc(event.target.value);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPostData({ ...postData, [e.target.name!]: e.target.value });
   };
 
   return (
     <form action="/posts" id="NewPostForm" noValidate autoComplete="off">
-      <div>
-        <TextField
-          id="Title"
-          label="Title"
-          multiline
-          fullWidth
-          variant="outlined"
-          onChange={handleTitle}
-        />
-      </div>
-      <br />
-      <div>
-        <TextField
-          id="Description"
-          label="Description"
-          multiline
-          fullWidth
-          rows={4}
-          variant="outlined"
-          onChange={handleDesc}
-        />
-        <input
-          accept="image/*, video/*, .pdf,.doc"
-          id="inputFiles"
-          multiple
-          type="file"
-        />
-        <Button
-          type="submit"
-          variant="contained"
-          onClick={() => {
-            alert(title + desc);
-          }}
-        >
-          {" "}
-          Post{" "}
-        </Button>
-      </div>
+      <Grid container spacing={2} alignItems="center" justify="center">
+        <Grid item xs={12}>
+          <TextField
+            id="Title"
+            label="Title"
+            multiline
+            fullWidth
+            variant="outlined"
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            id="Description"
+            label="Description"
+            multiline
+            fullWidth
+            rows={4}
+            variant="outlined"
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <input
+            accept="image/*, video/*, .pdf,.doc"
+            id="inputFiles"
+            multiple
+            type="file"
+          />
+        </Grid>
+      </Grid>
+      <br></br>
+      <Button type="submit" variant="contained" color="primary">
+        New Post
+      </Button>
     </form>
   );
 };
