64c4061371b91feab3e35ae0da32d5b8250eadd4
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-12-01 13:16:17 -0500

    fixed handleChange and post data

--- client/src/utils/posts.ts
+++ client/src/utils/posts.ts
@@ -23,6 +23,7 @@ export interface PostData {
  * @param courseId course id which posts belong to
  */
 
+/* May not need... */
 const courseExists = async (courseId: string) => {
   const courseRef = db.collection("courses").doc(courseId);
 
@@ -41,7 +42,9 @@ const courseExists = async (courseId: string) => {
   }
 };
 
+// TODO: Reduce calls from two -> one
 export const getPosts = async (uid: string, courseId: string) => {
+  // Check if course exists
   if (courseExists(courseId)) {
     try {
       const postsRef = db
@@ -50,7 +53,9 @@ export const getPosts = async (uid: string, courseId: string) => {
         .collection("posts");
       const postsSnap = await postsRef.get();
 
+      // Check if posts collection exists
       if (postsSnap.size > 0) {
+        //TODO: update to array return
         postsSnap.forEach((doc) => {
           console.log(doc.id, "=>", doc.data());
         });
@@ -63,6 +68,29 @@ export const getPosts = async (uid: string, courseId: string) => {
   }
 };
 
-export const addPostToPosts = async (postData: PostData) => {
-  //TODO
+// TODO: Reduce calls from three -> one
+export const addPostToPosts = async (
+  uid: string,
+  courseId: string,
+  postData: PostData
+) => {
+  // Check if course exists
+  if (courseExists(courseId)) {
+    try {
+      const postsRef = db
+        .collection("courses")
+        .doc(courseId)
+        .collection("posts");
+
+      console.log("got postsref");
+      // Add a new document with a generated id.
+      const res = await postsRef.add(postData);
+      console.log("got res");
+
+      // Sanity Check
+      // console.log('Added document with ID: ', res.id);
+    } catch (error) {
+      console.log("Error, could not add post :O");
+    }
+  }
 };
