b32fdc49901f76b454679738dc945f669ace8488
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-12-03 11:40:10 -0500

    added post logic passed from courses to display

--- client/src/pages/Post/index.tsx
+++ client/src/pages/Post/index.tsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "@material-ui/core/Container";
+import { PostData } from "utils/posts";
+import { getComments, CommentData } from "utils/comments";
 
-const Post: React.FunctionComponent = (props) => {
+type PostProps = { /*courseId: string, postId: string,*/ post: PostData };
+const Post: React.FunctionComponent<PostProps> = ({
+  /*courseId, postId,*/ post,
+}: PostProps) => {
+  const { author, title, description, links } = post;
+  const [comments, setComments] = useState<CommentData[]>([]);
+
+  // TODO: ADD RETURN EMPTY FOR NO COMMENTS CASE
+  // const getComments = async() => {
+  //   const commentsData = await getComments(courseId, postId);
+  // }
+
   return (
     <React.Fragment>
       <Container>
-        <h2>{/*props.title*/}</h2>
-        <p>{/*props.desc*/}</p>
-        <div>{/*props.files*/}</div>
+        <h2>{title}</h2>
+        <p>{author}</p>
+        <p>{description}</p>
+        <div>{links}</div>
+        {/* Show Comments conditional Rendering */}
       </Container>
     </React.Fragment>
   );
